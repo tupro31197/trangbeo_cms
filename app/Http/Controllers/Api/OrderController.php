a0bdfcc551225719238ad47392ee68eb49a8a2a9
truongnq.dev <truongdh1903@gmail.com>
2021-08-25 17:31:19 +0700

    search for order by order_code

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -20,8 +20,13 @@ class OrderController extends ControllerBase
                     'token' => $token,
                 ],
             ]);
-            $data = $client->get($this->urlAPI() . '/list-order?page=' . $page.'&status='.$status);
-           
+            $url = $this->urlAPI() . '/list-order?page=' . $page . '&status=' . $status;
+
+            if ($request->order_code) {
+                $url = $url . '&search=' . $request->order_code;
+            }
+            $data = $client->get($url);
+
             $response = json_decode($data->getBody()->getContents(), true);
             $orders = $response['data'];
             if ($response['status'] == 0) {
@@ -38,40 +43,40 @@ class OrderController extends ControllerBase
     public function updateOrder(Request $request, $code)
     {
         // try {
-            $token = $request->cookie('token');
+        $token = $request->cookie('token');
 
-            $body = [
-                'order_code' => $code,
-            ];
-            $input = json_encode($body);
-            $client = new Client([
-                'headers' => [
-                    'Content-Type' => 'application/json',
-                    'token' => $token,
-                ],
-            ]);
-            $status = $request->status;
-            $url = '';
-            if ($status == 4) {
-                $url = $this->urlAPI() . '/store-confirm-order';
-            } elseif ($status == 5) {
-                $url = $this->urlAPI() . '/finish-order';
-            } elseif ($status == 8) {
-                $url = $this->urlAPI() . '/cancel-order';
-            }
-            $req = $client->post($url, ['body' => $input]);
-            $response = json_decode($req->getBody()->getContents(), true);
-          
-            if ($response['status'] == 0) {
-                alert()->warning($response['message']);
-                return back();
-            }
-            alert()->success($response['message']);
-            $url = redirect()
-                ->route('order.listOrder', ['page' => 1, 'status' => 0])
-                ->getTargetUrl();
+        $body = [
+            'order_code' => $code,
+        ];
+        $input = json_encode($body);
+        $client = new Client([
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'token' => $token,
+            ],
+        ]);
+        $status = $request->status;
+        $url = '';
+        if ($status == 4) {
+            $url = $this->urlAPI() . '/store-confirm-order';
+        } elseif ($status == 5) {
+            $url = $this->urlAPI() . '/finish-order';
+        } elseif ($status == 8) {
+            $url = $this->urlAPI() . '/cancel-order';
+        }
+        $req = $client->post($url, ['body' => $input]);
+        $response = json_decode($req->getBody()->getContents(), true);
 
-            return redirect($url);
+        if ($response['status'] == 0) {
+            alert()->warning($response['message']);
+            return back();
+        }
+        alert()->success($response['message']);
+        $url = redirect()
+            ->route('order.listOrder', ['page' => 1, 'status' => 0])
+            ->getTargetUrl();
+
+        return redirect($url);
         // } catch (\Throwable $th) {
         //     alert($th)->error('Hệ thống đang được bảo trì. Vui lòng thử lại sau!');
         //     return back();
